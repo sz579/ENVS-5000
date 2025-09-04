275d5ab6572e20a3bcc242873a3adf9cc8d22112
sz579 <siqi.zhao@yale.edu>
2025-09-04 10:29:05 -0400

    Add files via upload

--- Practice Code - Sept 4.py
+++ Practice Code - Sept 4.py
@@ -4,31 +4,18 @@ import math
 import time
 
 #Define Initial Conditions and system factors#
-y0 = 0.0
-y0dot = 1.0
-zeta = 0.5
-omega0 = 1.0
+y0 = float(input('What is the initial position?\n'))
+y0dot = float(input('What is the initial velocity?\n'))
+zeta = float(input('What is the zeta value? Note: zeta should be between 0 and 1\n'))
+omega0 = float(input('What is the omega0 value?\n'))
 
 
 omegad = omega0 * math.sqrt(-zeta**2+1)
 A = y0
 B = (y0dot+zeta*omega0*y0)/omegad
 y = []
-
 t = np.linspace(0,20,1000)
-t1 = time.time()
-for i in range(0,1000,1):
-    y.append(math.exp(-zeta*omega0*t[i])*(A*math.cos(omegad*t[i])+B*math.sin(omegad*t[i])))
-
-plt.plot(t,y)
-plt.show()
-t2 = time.time()
-print(t2-t1)
-
-y = []
-t1=time.time()
 y = np.exp(-zeta*omega0*t)*(A*np.cos(omegad*t)+B*np.sin(omegad*t))
 plt.plot(t,y)
 plt.show()
 t2=time.time()
-print(t2-t1)
